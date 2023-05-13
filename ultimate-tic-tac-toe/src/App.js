a6c2eea80e26fdd42aa9877736fc0cd43bd93183
Cindy Lee <leecindy405@gmail.com>
2023-05-13 15:05:01 -0400

    adding mobile styles for player selection

--- ultimate-tic-tac-toe/src/App.js
+++ ultimate-tic-tac-toe/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Game from './components/Game';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Game />
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="gameContainer">
+    <Game />
+  </div>
+);
 
 export default App;
